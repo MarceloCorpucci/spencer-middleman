a3b40071e49cf90f19ea00e6d3ab4d1766c24168
Marcelo Corpucci <mcorpucci@gmail.com>
2020-04-27 08:59:46 -0300

    Added health check scenario.

--- features/steps/sanity.py
+++ features/steps/sanity.py
@@ -4,14 +4,15 @@ from api import main
 
 @given(u'the app has a sanity check')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the app has a sanity check')
+	main.app.config['TESTING'] = True
 
 
 @when(u'I make a request')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I make a request')
+	with main.app.test_client() as client:
+		context.response = client.get('/')
 
 
 @then(u'I should get an "OK" message and "200" status code')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should get an "OK" message and "200" status code')
+	assert b'{"message": "ok"}' in context.response.data.splitlines()
